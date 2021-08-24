074ad24013a035fee322ea5abde90c60fde8835c
lawdcoder <lawdcoder@gmail.com>
2021-08-24 15:54:20 -0400

    add restaurants

---
+++ restaurants/src/main/java/com/github/lawdcoder/restaurants/RestaurantsApplication.java
@@ -0,0 +1,13 @@
+package com.github.lawdcoder.restaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsApplication.class, args);
+	}
+
+}
