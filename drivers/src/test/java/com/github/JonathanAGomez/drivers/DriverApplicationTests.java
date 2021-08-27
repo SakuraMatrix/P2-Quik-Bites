fa13d8579e9a275e46bb536276f82ebd440b5de6
Jonathan Gomez <officialjonathangomez@hotmail.com>
2021-08-27 09:27:48 -0500

    file name change

--- drivers/src/test/java/com/github/JonathanAGomez/drivers/DriverApplicationTests.java
+++ drivers/src/test/java/com/github/JonathanAGomez/drivers/DriverApplicationTests.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DriversApplicationTests {
+class DriverApplicationTests {
 
 	@Test
 	void contextLoads() {
