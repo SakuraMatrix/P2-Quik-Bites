28f8fabf76663eb8f5af674633693dc86565cbac
Arenia <tyler.bagala@gmail.com>
2021-09-01 12:13:39 -0400

    Orders added to gateway service

--- orders/src/main/java/com/github/arenia/orders/controller/OrderController.java
+++ orders/src/main/java/com/github/arenia/orders/controller/OrderController.java
@@ -16,7 +16,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("")
 public class OrderController {
   private final OrderService orderService;
   
