40ab157cfe39137fa3b739db5a6108c705e2f9ea
Arenia <tyler.bagala@gmail.com>
2021-09-02 11:36:12 -0400

    Minor tweak to driver app

--- drivers/src/main/java/com/github/JonathanAGomez/drivers/DriverApplication.java
+++ drivers/src/main/java/com/github/JonathanAGomez/drivers/DriverApplication.java
@@ -2,8 +2,10 @@ package com.github.JonathanAGomez.drivers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DriverApplication {
 
 	public static void main(String[] args) {
